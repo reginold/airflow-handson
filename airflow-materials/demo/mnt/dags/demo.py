10d631827d5a9e1f22d9a6602e1010baafb3dafd
lu bowen <reginold.lu@gmail.com>
2021-09-22 00:13:01 +0900

    fix: change the base url

--- airflow-materials/demo/mnt/dags/demo.py
+++ airflow-materials/demo/mnt/dags/demo.py
@@ -9,7 +9,7 @@ from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
 
 default_args = {
-    "start_date": datetime(2021, 9, 14),
+    "start_date": datetime(2021, 9, 18),
     "owner": "RDL",
     "email": "owner@test.com"
     # "depends_on_past": False,
@@ -26,20 +26,20 @@ default_args = {
 
 
 def download_rates():
-    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+    BASE_URL = "https://gist.github.com/reginold/47c6645978cf158812a57aa27481e3fb/raw/"
     ENDPOINTS = {
-        "USD": "api_forex_exchange_usd.json",
-        "EUR": "api_forex_exchange_eur.json",
+        "USD": "api_exchange_usd.json",
+        "EUR": "api_exchange_eur.json",
     }
-    with open("/usr/local/airflow/dags/files/jobCode_ref.csv") as jobCode_ref:
-        reader = csv.DictReader(jobCode_ref, delimiter=";")
+    with open("/usr/local/airflow/dags/files/base_pair.csv") as base_pair:
+        reader = csv.DictReader(base_pair, delimiter=";")
         for idx, row in enumerate(reader):
             base = row["base"]
             with_pairs = row["with_pairs"].split(" ")
             indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
-            outdata = {"base": base, "rates": {}, "last_update": indata["date"]}
+            outdata = {"base": base, "rate": {}, "last_update": indata["date"]}
             for pair in with_pairs:
-                outdata["rates"][pair] = indata["rates"][pair]
+                outdata["rate"][pair] = indata["rate"][pair]
             with open("/usr/local/airflow/dags/files/rates.json", "a") as outfile:
                 json.dump(outdata, outfile)
                 outfile.write("\n")
@@ -47,23 +47,23 @@ def download_rates():
 
 with DAG(dag_id="demo", schedule_interval="@daily", default_args=default_args) as dag:
 
-    # Task 1: check the API is available or not
-    # reference site:https://airflow.apache.org/docs/apache-airflow-providers-http/stable/operators.html
+    # Task 1: check the API is available
+    # ref:https://airflow.apache.org/docs/apache-airflow-providers-http/stable/operators.html
     is_demo_api_available = HttpSensor(
         task_id="is_demo_api_available",
         method="GET",
         http_conn_id="demo_api",
-        endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
-        response_check=lambda response: "rates" in response.text,
+        endpoint="reginold/47c6645978cf158812a57aa27481e3fb",
+        response_check=lambda response: "rate" in response.text,
         # Time in seconds that the job should wait in between each tries
         poke_interval=5,
         timeout=20,
     )
-    # Task 2: check the FILE is avaliable or not
-    is_jobCode_ref_file_available = FileSensor(
-        task_id="is_jobCode_ref_file_available",
-        fs_conn_id="jobCode_ref_path",
-        filepath="jobCode_ref.csv",
+    # Task 2: check the file is avaliable
+    is_base_pair_file_available = FileSensor(
+        task_id="is_base_pair_file_available",
+        fs_conn_id="base_pair_path",
+        filepath="base_pair.csv",
         poke_interval=5,
         timeout=20,
     )
@@ -72,4 +72,4 @@ with DAG(dag_id="demo", schedule_interval="@daily", default_args=default_args) a
         task_id="downloading_rates", python_callable=download_rates
     )
 
-    is_demo_api_available >> is_jobCode_ref_file_available >> downloading_rates
+    is_demo_api_available >> is_base_pair_file_available >> downloading_rates
