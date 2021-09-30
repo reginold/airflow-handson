88bc721244ce12881190b7bd65e6ddd108358a1a
lu bowen <reginold.lu@gmail.com>
2021-09-30 11:51:11 +0900

    fix: Change the config file for start up container base on revert

--- airflow-materials/demo/start.sh
+++ airflow-materials/demo/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Startup all the containers without cache
-docker-compose rm --all &&
-docker-compose pull &&
-docker-compose build --no-cache &&
-docker-compose up -d --force-recreate
-
 # Startup all the containers at once
-# docker-compose up -d --build
+docker-compose up -d --build
