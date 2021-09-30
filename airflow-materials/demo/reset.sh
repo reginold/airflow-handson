fa0aeb3715892496509a6173f9bf28f4520ebe7a
lu bowen <reginold.lu@gmail.com>
2021-09-30 18:12:46 +0900

    fix: Add the requirements file

--- airflow-materials/demo/reset.sh
+++ airflow-materials/demo/reset.sh
@@ -1,7 +1,13 @@
+# Startup all the containers without cache
+docker-compose rm --all &&
+docker-compose pull &&
+docker-compose build --no-cache &&
+docker-compose up -d --force-recreate
+
 # /!\ WARNING: RESET EVERYTHING! 
 # Remove all containers/networks/volumes/images and data in db
-docker-compose down
-docker system prune -f
-docker volume prune -f
-docker network prune -f
-docker rmi -f $(docker images -a -q)
+# docker-compose down
+# docker system prune -f
+# docker volume prune -f
+# docker network prune -f
+# docker rmi -f $(docker images -a -q)
